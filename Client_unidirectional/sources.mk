ee944747255c12dac9da415bd06623646a2ce5a1
Vixerunt <146630867+Vixerunt506@users.noreply.github.com>
2024-03-15 14:37:28 +0000

    Added files

---
+++ Client_unidirectional/sources.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+POST_BUILD_OBJCOPY := 
+OBJS := 
+ASM_DEPS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+POST_BUILD_PRINT_SIZE := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+HAL \
+Library \
+Platform_Layers \
+Stack \
+Startup \
+User \
+
